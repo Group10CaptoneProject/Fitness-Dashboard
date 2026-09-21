b944a303ff2502f86c74b6c277288d05c63b1ce1
Leozheng04 <leozheng1204@gmail.com>
2026-09-21 19:12:13 -0400

    Finished route for register

---
+++ backend/src/app/schemas/auth.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel , EmailStr, Field
+
+class RegisterUser(BaseModel):
+    username: str = Field(min_length=3, max_length=50)
+    email: EmailStr = Field(max_length=100)
+    first_name: str = Field(min_length = 1, max_length=50)
+    last_name: str = Field(min_length = 1, max_length=50)
+    password: str = Field(min_length=6, max_length=255)
+
+class LoginUser(BaseModel):
+    email: EmailStr = Field(max_length=100)
+    password: str = Field(min_length=6, max_length=255)
